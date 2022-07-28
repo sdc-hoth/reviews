e2cdfdb6c6613a8495915ef6cdfbb05a0d39316b
dkroll713 <dkroll713@gmail.com>
2022-07-28 14:45:37 -0500

    fleshing out query structure

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS reviewsMeta;
 --   "id" SERIAL PRIMARY KEY
 -- );
 
-DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS reviews CASCADE;
 
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
@@ -27,7 +27,7 @@ CREATE TABLE reviews (
 -- delimiter ','
 -- CSV HEADER;
 
-DROP TABLE IF EXISTS reviewPhotos;
+DROP TABLE IF EXISTS reviewPhotos CASCADE;
 
 CREATE TABLE IF NOT EXISTS reviewPhotos (
     id SERIAL PRIMARY KEY,
@@ -40,128 +40,44 @@ CREATE TABLE IF NOT EXISTS reviewPhotos (
 -- delimiter ','
 -- CSV HEADER;
 
--- DROP TABLE IF EXISTS characteristics;
+DROP TABLE IF EXISTS characteristics CASCADE;
 
--- CREATE TABLE characteristics (
---     "id" SERIAL PRIMARY KEY,
--- )
+CREATE TABLE characteristics (
+    id SERIAL PRIMARY KEY,
+    product_id integer,
+    name varchar(10)
+);
+
+-- COPY characteristics(id,product_id,name)
+-- FROM '/home/david/hackreactor/sdc/reviews/csv/characteristics.csv'
+-- delimiter ','
+-- CSV HEADER;
+
+DROP TABLE IF EXISTS characteristicsReviews CASCADE;
+
+CREATE TABLE characteristicsReviews (
+    id SERIAL PRIMARY KEY,
+    characteristic_id integer,
+    review_id integer,
+    value integer
+);
+
+-- COPY characteristicsreviews(id,Characteristic_id, review_id,value)
+-- FROM '/home/david/hackreactor/sdc/reviews/csv/characteristic_reviews.csv'
+-- delimiter ','
+-- CSV HEADER;
 
 -- -- ---
 -- -- Foreign Keys
 -- -- ---
 
 ALTER TABLE reviewPhotos ADD FOREIGN KEY (review_id) REFERENCES reviews (id);
--- ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
-
--- CREATE DATABASE chat;
-
--- USE chat;
-
--- -- CREATE TABLE messages (
--- --   /* Describe your table here.*/
--- --   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- --   id_usernames INT NOT NULL,
--- --   id_rooms INT NOT NULL,
--- --   messageText VARCHAR(255)
--- -- );
-
--- -- /* Create other tables and define schemas for them here! */
-
--- -- CREATE TABLE usernames (
--- --   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- --   username VARCHAR(255) NOT NULL
--- -- );
-
--- -- CREATE TABLE rooms (
--- --   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- --   room VARCHAR(255) NOT NULL
--- -- );
-
--- -- -- ---
--- -- -- Foreign Keys
--- -- -- ---
-
--- -- ALTER TABLE `messages` ADD FOREIGN KEY (id_usernames) REFERENCES usernames (id);
--- -- ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES rooms (id);
-
--- -- INSERT INTO `messages` (`id`,`id_usernames`,`id_rooms`,`messageText`) VALUES
--- -- (0,0,0,'sup guys');
--- -- INSERT INTO `usernames` (`id`,`username`) VALUES
--- -- (0,'david');
--- -- INSERT INTO `rooms` (`id`,`room`) VALUES
--- -- (0,'lobby');
-
--- /*  Execute this file from the command line by typing:
---  *    mysql -u root < server/schema.sql
---  *  to create the database and the tables.*/
-
--- DROP TABLE IF EXISTS `messages`;
-
--- CREATE TABLE `messages` (
---   `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'the message id',
---   `id_usernames` INTEGER NOT NULL COMMENT 'the user id',
---   `id_rooms` INTEGER NOT NULL COMMENT 'the room id',
---   `messageText` VARCHAR(255) NOT NULL COMMENT 'the contents of the message submitted',
---   PRIMARY KEY (`id`)
--- ) COMMENT 'contains all messages sent to the server';
+-- ALTER TABLE characteristicsReviews ADD FOREIGN KEY (characteristic_id) REFERENCES characteristics (id);
+-- ALTER TABLE characteristicsReviews ADD FOREIGN KEY (review_id) REFERENCES reviews (id);
 
 -- -- ---
--- -- Table 'usernames'
--- -- holds each username submitted to the application
+-- -- SQL Queries
 -- -- ---
 
--- DROP TABLE IF EXISTS `usernames`;
-
--- CREATE TABLE `usernames` (
---   `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'the user id',
---   `username` VARCHAR(255) NOT NULL COMMENT 'the chosen username',
---   PRIMARY KEY (`id`)
--- ) COMMENT 'holds each username submitted to the application';
-
--- -- ---
--- -- Table 'rooms'
--- -- holds every room created
--- -- ---
-
--- DROP TABLE IF EXISTS `rooms`;
-
--- CREATE TABLE `rooms` (
---   `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'the room id',
---   `room` VARCHAR(255) NOT NULL COMMENT 'the chosen room',
---   PRIMARY KEY (`id`)
--- ) COMMENT 'holds every room created';
-
--- -- ---
--- -- Foreign Keys
--- -- ---
-
--- ALTER TABLE `messages` ADD FOREIGN KEY (id_usernames) REFERENCES `usernames` (`id`);
--- ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
-
--- -- ---
--- -- Table Properties
--- -- ---
-
--- -- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- -- ALTER TABLE `usernames` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- -- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- -- ---
--- -- Test Data
--- -- ---
-
--- -- INSERT INTO `usernames` (`id`,`username`) VALUES
--- -- (0,'David');
--- -- INSERT INTO `rooms` (`id`,`room`) VALUES
--- -- (0,'lobby');
--- -- INSERT INTO `messages` (`id`,`id_usernames`,`id_rooms`,`messageText`) VALUES
--- -- (0,1,1,'howdy yall');
--- -- INSERT INTO `usernames` (`id`,`username`) VALUES
--- -- (1,'Kai');
--- -- INSERT INTO `rooms` (`id`,`room`) VALUES
--- -- (1,'lobby');
--- -- INSERT INTO `messages` (`id`,`id_usernames`,`id_rooms`,`messageText`) VALUES
--- -- (1,1,1,'sup man');
-
--- -- query for finding usernames rooms, and messages:
--- -- select u.username, r.room, m.messageText from messages m inner join usernames u on u.id = m.id_usernames inner join rooms r on r.id = m.id_rooms;
+-- get average rating for product
+-- SELECT AVG(r.rating) FROM reviews r WHERE r.product_id = 13;
